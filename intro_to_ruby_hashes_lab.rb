32721e964c197c139aca1cbfc15c3a3ea7a5fd40
magical-flow-3186 <rregalado17@gmail.com>
2019-06-06 19:52:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,8 @@ def monopoly_with_second_tier
 	  :railroads => {
 	    :pieces => {values => 4},
 	    :names => {},
-	    :rent_in_dollars => {}
-	,}
-
+	    :rent_in_dollars => {},
+	}
 end
 
 def monopoly_with_third_tier
